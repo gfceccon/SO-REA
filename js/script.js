d3816a62fa59c1831dbb4f3865e807872dd4eef3
Gustavo Ceccon <gustavofceccon@live.com>
2015-10-05 15:20:52 -0300

    Hide, Show canvas

--- js/script.js
+++ js/script.js
@@ -1,91 +1,157 @@
-(function()
+var llCanvas = false;
+var disk = false;
+var actuator = false;
+var llSelector = false;
+var llRpm = false;
+var stopAnimation = false;
+
+function lowLevelFormatting()
 {
 	fabric.Object.prototype.originX = fabric.Object.prototype.originY = 'center';
 	var rpm = 4;
-	var disk;
-	var actuator;
-	var canvas = this.__canvas = new fabric.Canvas('canvas', 
+	stopAnimation = false;
+	if(llCanvas == false)
 	{
-		hoverCursor: 'pointer',
-		selection: false,
-		perPixelTargetFind: true,
-		targetFindTolerance: 5
-	});
+		llCanvas = new fabric.Canvas('canvas', 
+		{
+			hoverCursor: 'pointer',
+			selection: false,
+			perPixelTargetFind: true,
+			targetFindTolerance: 5
+		});
+	}
+	else
+	{
+		$("#canvas").show();
+	}
 
-	fabric.Image.fromURL('images/hard_disk.png', function(diskImg)
+	if(disk == false)
 	{
-		disk = diskImg;
-		disk.originX = disk.originY = 'center';
-		disk.lockMovementX = disk.lockMovementY = true;
-		disk.selectable = false;
-		disk.left = disk.top = 300;
-		canvas.add(diskImg);
+		fabric.Image.fromURL('images/hard_disk.png', function(diskImg)
+		{
+			disk = diskImg;
+			disk.originX = disk.originY = 'center';
+			disk.lockMovementX = disk.lockMovementY = true;
+			disk.selectable = false;
+			disk.left = disk.top = 300;
+			llCanvas.add(disk);
+			diskAnimate();
+		});
+	}
+	else
+	{
+		llCanvas.add(disk);
 		diskAnimate();
-	});
+	}
 
-	fabric.Image.fromURL('images/actuator_arm.png', function(actuatorImg)
+	if(actuator == false)
 	{
-		actuator = actuatorImg;
-		actuator.originX = actuator.originY = 'center';
-		actuator.lockMovementX = actuator.lockMovementY = true;
-		actuator.selectable = false;
-		actuator.left = 700;
-		actuator.top = 500;
-		canvas.add(actuatorImg);
+		fabric.Image.fromURL('images/actuator_arm.png', function(actuatorImg)
+		{
+			actuator = actuatorImg;
+			actuator.originX = actuator.originY = 'center';
+			actuator.lockMovementX = actuator.lockMovementY = true;
+			actuator.selectable = false;
+			actuator.left = 700;
+			actuator.top = 500;
+			llCanvas.add(actuator);
+			actuatorAnimate();
+		});
+	}
+	else
+	{
+		llCanvas.add(actuator);
 		actuatorAnimate();
-	});
+	}
 
 	function diskAnimate()
 	{
-		fabric.util.animate(
+		if(stopAnimation == false)
 		{
-			startValue: 0,
-			endValue: 360,
-			duration: 60000 / rpm ,
-
-			easing: function(t, b, c, d) { return c*t/d + b; },
-
-			onChange: function(angle)
+			fabric.util.animate(
 			{
-				disk.setAngle(angle);
-				canvas.renderAll();
-			},
-			onComplete: diskAnimate
-		});
+				startValue: 0,
+				endValue: 360,
+				duration: 60000 / rpm ,
+
+				easing: function(t, b, c, d) { return c*t/d + b; },
+
+				onChange: function(angle)
+				{
+					disk.setAngle(angle);
+					llCanvas.renderAll();
+				},
+				onComplete: diskAnimate
+			});
+		}
 	};
 	
 	function actuatorAnimate()
 	{
-		fabric.util.animate(
+		if(stopAnimation == false)
 		{
-			startValue: 60,
-			endValue: 105,
-			duration: 60000 / rpm ,
-
-			easing: function(t, b, c, d) { if(t > d/2) return c*t/d + b; else return c*(1 - t/d) + b },
-
-			onChange: function(angle)
+			fabric.util.animate(
 			{
-				actuator.setAngle(angle);
-				canvas.renderAll();
-			},
-			onComplete: actuatorAnimate
-		});
+				startValue: 60,
+				endValue: 105,
+				duration: 60000 / rpm ,
+
+				easing: function(t, b, c, d) { if(t > d/2) return c*t/d + b; else return c*(1 - t/d) + b },
+
+				onChange: function(angle)
+				{
+					actuator.setAngle(angle);
+					llCanvas.renderAll();
+				},
+				onComplete: actuatorAnimate
+			});
+		}
 	};
 
-	$( "#selector" ).slider(
+	if(llSelector == false)
 	{
-		min: 0,
-		max: 32,
-		value: 4,
-		step: 4,
-		slide: function( event, ui )
+		llSelector = $( "#selector" );
+		llRpm = $("#dur");
+		llSelector.slider(
 		{
-			rpm = ui.value;
-			$("#dur").html("" + rpm);
-      diskAnimate();
-      actuatorAnimate();
-		}
-	});
-	$("#dur").html("" + rpm);
-})();
+			min: 0,
+			max: 32,
+			value: 4,
+			step: 4,
+			slide: function( event, ui )
+			{
+				rpm = ui.value;
+				llRpm.html("" + rpm);
+				diskAnimate();
+				actuatorAnimate();
+			}
+		});
+		llRpm.html("" + rpm);
+	}
+	else
+	{
+		llSelector.show();
+		llRpm.show();
+	}
+}
+
+function hideAll()
+{
+	if(llCanvas != false)
+	{
+		$("#canvas").hide();
+		llSelector.hide();
+		llRpm.hide();
+		llCanvas.remove(disk);
+		llCanvas.remove(actuator);
+	}
+	
+	$("#text").html("");
+	
+	stopAnimation = true;
+}
+
+function about()
+{
+	$("#text").html("Fuck you!");
+}
